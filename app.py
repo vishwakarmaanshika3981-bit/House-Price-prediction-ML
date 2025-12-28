c7bbdd14f5b103fa08f08b125306deeea0064b31
vishwakarmaanshika3981-bit <vishwakarmaanshika3981@gmail.com>
2025-12-28 15:40:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,11 @@ st.sidebar.title('🏠House Price prediction using ML')
 st. sidebar.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrEZFyB3eYVFx4fiwANh-wSdkFGVBmth2Gsg&s')
 all_value=[]
 for i in X:
-    min_value = int(X[i].min())
-    max_value = int(X[i].max())
+    min_value = int (X[i].min())
+    max_value = int (X[i].max())
     ans =st.sidebar.slider(f'select{i} value',min_value,max_value)
     all_value.append(ans)
 # st.write(all_value)
+
 
 
