6d7137e2b2c227309b47a4b9fb155f46ec3419d3
vishwakarmaanshika3981-bit <vishwakarmaanshika3981@gmail.com>
2025-12-28 16:39:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,45 +5,40 @@ import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
 from sklearn.ensemble import RandomForestRegressor
-st.title('🏠House Price prediction using ML')
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrEZFyB3eYVFx4fiwANh-wSdkFGVBmth2Gsg&s')
-df=pd.read_csv('house_data.csv')
-X=df.iloc[:, :-3]
-y=df.iloc[:, -1]
-st.sidebar.title('🏠House Price prediction using ML')
-st. sidebar.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrEZFyB3eYVFx4fiwANh-wSdkFGVBmth2Gsg&s')
-all_value=[]
+st.title('🏠 House Price prediction using ML')
+st.image('https://i.pinimg.com/originals/f1/be/c8/f1bec81e20d80cd36c82379af920a4e9.gif')
+
+df = pd.read_csv('house_data.csv')
+X = df.iloc[:,:-3]
+y = df.iloc[:,-1]
+
+st.sidebar.title('🏠 Select House features')
+st.sidebar.image('https://i.pinimg.com/originals/f1/be/c8/f1bec81e20d80cd36c82379af920a4e9.gif')
+all_value = []
 for i in X:
-    min_value = int (X[i].min())
-    max_value = int (X[i].max())
-    ans =st.sidebar.slider(f'select {i} value',min_value,max_value)
-    all_value.append(ans)
+  min_value = int(X[i].min())
+  max_value = int(X[i].max())
+  ans = st.sidebar.slider(f'Select {i} value',min_value, max_value)
+  all_value.append(ans)
+
 # st.write(all_value)
+
 scaler = StandardScaler()
 scaled_X = scaler.fit_transform(X)
 final_value = scaler.transform([all_value])
+
 @st.cache_data
-def model_run():
-model = (RandomForestRegressor)
-model.fit(X,y)
-return model
-model =model_run[X,y]
+def model_run(X,y):
+  model = RandomForestRegressor()
+  model.fit(X,y)
+  return model
+
+model = model_run(X,y)  
 house_price = model.predict(final_value)[0]
+
 with st.spinner('Predicting House price'):
-    time.sleep(1)
-    msg= f'''House price is : $ {round(house_price*100000,2)}'''
-    st.success(msg)
-    st.markdown('''**design and developed by:Anshika**''')
+  time.sleep(1)
+msg = f'''House price is: $ {round(house_price*100000,2)}'''
+st.success(msg)
 
-
-
-
-
-
-
-
-
-
-
-
-
+st.markdown('''**Design and Developed by: Ankush verma**''')
